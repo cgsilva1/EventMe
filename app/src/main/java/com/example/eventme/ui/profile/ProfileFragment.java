3d56326b76e6f432ec7c7012fdbb5b69727529e7
Caroline Silva <cgsilva@usc.edu>
2022-11-04 20:45:48 -0700

    updated profile page

--- app/src/main/java/com/example/eventme/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/eventme/ui/profile/ProfileFragment.java
@@ -1,26 +1,63 @@
 package com.example.eventme.ui.profile;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import com.example.eventme.R;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.eventme.ui.explore.ExploreAdapter;
+import com.example.eventme.ui.explore.ExploreFragment;
 import com.example.eventme.ui.register.Register;
 import com.example.eventme.databinding.FragmentProfileBinding;
 import com.example.eventme.ui.login.LoginActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
+import java.util.Objects;
 
 public class ProfileFragment extends Fragment {
+    View promptA;
+    View promptB;
     Button loginButton;
     Button createAccountButton;
+
+    String userID; //id in database
+    TextView name;
+    ImageView profileImage;
+    TextView dob;
+    Button uploadPhotoButton;
+    Button signOut;
+    ImageView profilePic;
+    ImageView logo;
+    String uname;
+    String udob;
+    String userId;
+
     private FragmentProfileBinding binding;
+    FirebaseAuth mAuth;
+    FirebaseFirestore fStore;
+    StorageReference storageReference;
+    boolean userLoggedIn = true; //user not signed in
+
+
+//    TO DO:
+//        IF(SIGNED IN) -> SHOW PROFILE INFORMATION
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,23 +67,90 @@ public class ProfileFragment extends Fragment {
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        //LOGIN BUTTON ONCLICK
-        loginButton = root.findViewById(R.id.loginButton);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendLogin();
-            }
-        });
+        //display info for profile WHEN LOGGED IN
+        profileImage = root.findViewById(R.id.profile_image);
+        name = root.findViewById(R.id.name_profile);
+        dob = root.findViewById(R.id.dob_profile);
+        //upload profile picture button
+        uploadPhotoButton = root.findViewById(R.id.uploadPhotoBtn);
+        //sign out button
+        signOut = root.findViewById(R.id.signOutBtn);
+        profilePic = root.findViewById(R.id.profile_image);
 
-        //REGISTER BUTTON ONCLICK
+        //info when NOT LOGGED IN
+        //promts
+        promptA = root.findViewById(R.id.prompt1);
+        promptB = root.findViewById(R.id.prompt2);
+        logo = root.findViewById(R.id.logo);
+        //login button
+        loginButton = root.findViewById(R.id.loginButton);
+        //create account button
         createAccountButton = root.findViewById(R.id.createAccountButton);
-        createAccountButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendRegister();
-            }
-        });
+
+        if(loggedIn()){ //if user is logged in show profile infomration & log out button
+
+            promptA.setVisibility(View.GONE);
+            promptB.setVisibility(View.GONE);
+            logo.setVisibility(View.GONE);
+            loginButton.setVisibility(View.GONE);
+            createAccountButton.setVisibility(View.GONE);
+
+            mAuth = FirebaseAuth.getInstance();
+            fStore = FirebaseFirestore.getInstance();
+            storageReference = FirebaseStorage.getInstance().getReference();
+//            final StorageReference profileReference = storageReference.child("Users/"+ Objects.requireNonNull(mAuth.getCurrentUser()).getUid()+"/profile.jpg");
+//            profileReference.getDownloadUrl().addOnSuccessListener(uri -> Picasso.get().load(uri).into(profileImage));
+
+            //userID = Objects.requireNonNull(mAuth.getCurrentUser()).getUid();
+            //DocumentReference documentReference = fStore.collection("Users").document(userID);
+//            documentReference.get().addOnSuccessListener(value -> {
+//                uname = value.getString("name");
+//                name.setText(uname);
+//                udob = value.getString("dob");
+//                dob.setText(udob);
+//                userId=userID;
+//            });
+
+
+            //UPLOAD PHOTO ONCLICK
+            uploadPhotoButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    sendUploadPicture();
+                }
+            });
+
+            //SIGN OUT ONCLICK
+            signOut.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    sendSignOut();
+                }
+            });
+        }
+        else{ //User is not logged in
+            profilePic.setVisibility(View.GONE);
+            name.setVisibility(View.GONE);
+            dob.setVisibility(View.GONE);
+            uploadPhotoButton.setVisibility(View.GONE);
+            signOut.setVisibility(View.GONE);
+
+            //LOGIN BUTTON ONCLICK
+            loginButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    sendLogin();
+                }
+            });
+
+            //REGISTER BUTTON ONCLICK
+            createAccountButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    sendRegister();
+                }
+            });
+        }
 
         final TextView textView = binding.textProfile;
         profileViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
@@ -63,10 +167,26 @@ public class ProfileFragment extends Fragment {
         startActivity(intent);
     }
 
-    public void loggedIn(){
-
+    public boolean loggedIn(){
+        if(mAuth.getInstance().getCurrentUser() != null){ //user is signed in
+            userLoggedIn = true;
+        }
+        return false;
     }
 
+    public void sendUploadPicture(){
+        // open gallery on phone
+        // this intent returns the image that the user has clicked on to select
+        Intent openGalleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+         startActivityForResult(openGalleryIntent, 1000);
+    }
+
+    public void sendSignOut(){
+//        mAuth.signOut();
+        userLoggedIn = true;
+        Intent intent = new Intent(getActivity(), ExploreFragment.class); //send back to explore page
+        startActivity(intent);
+    }
 
     @Override
     public void onDestroyView() {
